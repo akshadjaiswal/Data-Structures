d77095d5c5864cf7430df5cdc9a673ecc0cdc65d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-25 15:27:34 +0530

    update

--- 02-Sum-of-Two-Numbers/main.java
+++ 02-Sum-of-Two-Numbers/main.java
@@ -3,9 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         int x = 89; 
-        
         int y = 43;
-        
         int sum = x + y;
         
         System.out.println("Sum is " + sum);
