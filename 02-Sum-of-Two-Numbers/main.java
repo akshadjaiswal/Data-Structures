98f5d0680eba3f36aed4456b7989557387aaafb1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-25 15:26:59 +0530

    update

---
+++ 02-Sum-of-Two-Numbers/main.java
@@ -0,0 +1,14 @@
+package main;
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 89; 
+        
+        int y = 43;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+    
+}
