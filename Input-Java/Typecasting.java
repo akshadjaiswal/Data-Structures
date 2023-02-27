c98c620705c0176aff1a0dc8a540c4e85ecc09b4
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-27 17:33:42 +0530

    comment

--- Input-Java/Typecasting.java
+++ Input-Java/Typecasting.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Typecasting {
     public static void main(String[] args) {
         Scanner input=new Scanner (System.in);
+
+        //typecasting
         int num = (int)(54.25f); //It will covert the integer in to the float
         System.out.println(num);
     }
