240ef6b34f358c777b8e12141ab5561fb87c12c8
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-27 16:27:32 +0530

    conceptual coding

---
+++ Primitives-Java/main.java
@@ -0,0 +1,10 @@
+public class primitives {
+    public static void main(String[] args) {
+        int rollNo= 3516;
+        char letter = 'A';
+        float percentage = 98.62f;
+        long largeInteger = 5191894895982L;
+        boolean check = true;
+
+    }
+}
