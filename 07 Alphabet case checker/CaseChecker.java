e51b65e3b834d2899cfa39fb901e885f5e538508
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-28 20:25:07 +0530

    Alphabet case checker

---
+++ 07 Alphabet case checker/CaseChecker.java
@@ -0,0 +1,17 @@
+package com.akshad;
+
+import java.util.Scanner;
+
+public class CaseChecker {
+    public static void main(String[] args) {
+        //Q. Check whether the alphabet is lowercase or uppercase.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the word or alphabet to check case-");
+        char ch = in.next().trim().charAt(0);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lower-Case");
+        } else {
+            System.out.println("Upper-Case");
+        }
+    }
+}
