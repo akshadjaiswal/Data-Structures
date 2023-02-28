9fb10b187da0d77a56ed6dc3f67089bd91fa3171
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-28 19:19:33 +0530

    problem using if

---
+++ 06 Maximum between three numbers/Largest.java
@@ -0,0 +1,22 @@
+package com.akshad;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        //Q.Find maximum between three numbers
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        // Logic section using if
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
