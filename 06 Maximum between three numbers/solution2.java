00f847d21599fff2c0824c182726ab26f86f8447
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-28 19:39:20 +0530

    another solution of max between three numbers

---
+++ 06 Maximum between three numbers/solution2.java
@@ -0,0 +1,24 @@
+package com.akshad;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        //Q.Find maximum between three numbers
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int max=0;
+        if(a>b){
+            max=a;
+        }else{
+            max=b;
+        }
+        if(c>max){
+            max=c;
+        }
+        System.out.println(max);
+    }
+}
