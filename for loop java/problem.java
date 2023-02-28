e12bb0517c0ca5c79178f1d176437098ab54817d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-28 14:19:24 +0530

    for loop problem update

---
+++ for loop java/problem.java
@@ -0,0 +1,21 @@
+public class Problem {
+    public static void main(String[] args) {
+        /*
+     Q.Print number from 1 to 5
+      */
+
+        // for (int num = 1; num <= 5; num++) {
+        //     System.out.println(num);
+        // }
+
+        // Q.print number from 1 to n
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the nth value for printing a number");
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
