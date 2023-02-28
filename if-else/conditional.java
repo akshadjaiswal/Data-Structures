c656f2f2bd026e063a62c493faa4ba4825d56222
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-28 12:53:38 +0530

    comditionals

---
+++ if-else/conditional.java
@@ -0,0 +1,15 @@
+public class Conditional {
+    public static void main(String[] args) {
+
+        //if else statement
+        int salary = 45000;
+        if (salary > 50000) {
+            salary = salary + 2000;
+           
+        } else {
+            salary = salary + 1000;
+            
+        }
+            System.out.println(salary);
+    }
+}
