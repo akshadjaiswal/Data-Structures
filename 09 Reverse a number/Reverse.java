9ee049397a54bf4e76d1a2b40f8485de0ceec6c3
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-01 15:25:12 +0530

    adding comment

--- 09 Reverse a number/Reverse.java
+++ 09 Reverse a number/Reverse.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Reverse {
     public static void main(String[] args) {
-
+        // Q.Find a reverse of a number.
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number to reverse:");
         int num = in.nextInt();
