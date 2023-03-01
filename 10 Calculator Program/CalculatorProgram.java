513ecd92e738c03b62f2d55cd2041f14e8484d4b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-01 16:30:25 +0530

    comments to calculator program

--- 10 Calculator Program/CalculatorProgram.java
+++ 10 Calculator Program/CalculatorProgram.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 
 public class CalculatorProgram {
     public static void main(String[] args) {
+        //Q.Create a simple calculator program with addition , substractions, multiplication,dividation and modulus.
         System.out.println("Calculator Program");
-        System.out.println();
         Scanner in = new Scanner(System.in);
         int ans = 0;
         while(true){
         System.out.println("Enter operation (+,-,*,/,%)");
+        //taking operator as a input
         char op = in.next().trim().charAt(0);
         if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+            //taking input of two number
             System.out.println("Enter two number-");
             int num1 = in.nextInt();
             int num2 = in.nextInt();
