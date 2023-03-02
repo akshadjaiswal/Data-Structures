203d6a5acfd3a92648c40d07bad97ac6caeb5995
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-02 15:15:37 +0530

    greet by method

---
+++ Functions methods java/Greetings.java
@@ -0,0 +1,8 @@
+public class Greetings {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        System.out.println("Hello its me Akshad");
+    }
+}
