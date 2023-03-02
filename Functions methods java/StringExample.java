14c3cdaf3d1e6132f83fa6b7b5c4689ae452b4ee
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-02 17:39:11 +0530

    Passing string as a parameter.

--- Functions methods java/StringExample.java
+++ Functions methods java/StringExample.java
@@ -1,9 +1,18 @@
 public class StringExample {
     public static void main(String[] args) {
-        String ans = greet();
-        System.out.println(ans);
+        // String ans = greet();
+        // System.out.println(ans);
+        
+        String personalized=mygreet("Akshad Jaiswal");
+        System.out.println(personalized);
+    }
+    //passing parameter function
+     static String mygreet(String name) {
+        String message = "Hello " + name;
+        return message;
     }
 
+    //return value
     static String greet() {
         String greet = "Happy Birthday";
         return greet;
