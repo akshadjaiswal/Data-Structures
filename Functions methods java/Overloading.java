fda72805495dd475c4bd4aaf25a66e325d8f765b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-03 15:54:01 +0530

    Method overloading

---
+++ Functions methods java/Overloading.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Overloading {
+    public static void main(String[] args) {
+        fun(999);
+        fun("JUICE WRLD");
+        int ans = sum(1, 2);
+        int ans2 = sum(1, 2, 2);
+        System.out.println(ans);
+        System.out.println(ans2);
+        jw(999);
+        jw("JUICE WRLD");
+    }
+
+    static void jw(int... v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void jw(String... v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static void fun(int a) {
+        System.out.println("First one-");
+        System.out.println(a);
+    }
+
+    static void fun(String name) {
+        System.out.println("Second one-");
+        System.out.println(name);
+
+    }
+}
