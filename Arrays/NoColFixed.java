e9689e3ebbfa5627e3cd992d512a05a5c2300842
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-04 16:41:24 +0530

    Printing matrix when size of column in not fix

---
+++ Arrays/NoColFixed.java
@@ -0,0 +1,16 @@
+public class NoColFixed {
+    public static void main(String[] args) {
+        //printing an matrix when the size of coloumn not fixed.
+        int arr[][] = {
+                {1, 2, 3},
+                {1, 2},
+                {1, 2, 3, 4}
+        };
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
