bce472d9f910af04bf5da00c8effb72fb161b59e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-04 17:13:13 +0530

    Array List Example

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the element to add");
+        ArrayList<Integer> list = new ArrayList<>(10);
+//
+
+//        System.out.println(list.contains(1555));
+
+//        list.set(0,99);
+//        System.out.println(list);
+//        list.remove(2);
+//        System.out.println(list);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        //get a item at any index
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i) + " ");
+        }
+        System.out.println(list);
+    }
+}
