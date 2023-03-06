d565f9a2c04ec781328c7ceba3d6de939588ff02
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-06 15:33:34 +0530

    Optimized solution for even digits

--- Arrays/EvenDigits.java
+++ Arrays/EvenDigits.java
@@ -3,8 +3,8 @@
 public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {12, 345, 2, 6, 7896};
-//        System.out.println(findNumbers(nums));
-        System.out.println(digits(0));
+       System.out.println(findNumbers(nums));
+        // System.out.println(digits(0));
     }
 
     static int findNumbers(int[] nums) {
@@ -19,25 +19,31 @@ public class EvenDigits {
 
     //function to check wether the number contains even digits or not
     static boolean even(int num) {
-        int numberOfDigits = digits(num);
+        int numberOfDigits = digits2(num);
        /* if (numberOfDigits % 2 == 0) return true;
         else return false;*/
         return numberOfDigits % 2 == 0;
     }
-
-    //count the number of the digitd
-    static int digits(int num) {
+    //optimized digit finding method
+    static int digits2(int num) {
         if (num < 0) {
             num = num * -1;
         }
-        if (num == 0) {
-            return 1;
-        }
-        int count = 0;
-        while (num > 0) {
-            count++;
-            num = num / 10;
-        }
-        return count;
+        return (int) (Math.log10(num)) + 1;
     }
+    //count the number of the digit
+    // static int digits(int num) {
+    //     if (num < 0) {
+    //         num = num * -1;
+    //     }
+    //     if (num == 0) {
+    //         return 1;
+    //     }
+    //     int count = 0;
+    //     while (num > 0) {
+    //         count++;
+    //         num = num / 10;
+    //     }
+    //     return count;
+    // }
 }
