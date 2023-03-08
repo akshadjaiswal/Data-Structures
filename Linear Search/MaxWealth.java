2bf0fc63e45625322b55d0614d749f5ed30c704e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-08 17:41:24 +0530

    Max Wealth Leets code question

---
+++ Linear Search/MaxWealth.java
@@ -0,0 +1,31 @@
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int show = maximumWealth(arr);
+        System.out.println(show);
+    }
+
+    static int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            if (sum > ans) ans = sum;
+        }
+        return ans;
+
+       /* for (int person = 0; person < accounts.length; person++) {
+            int sum = 0;
+            for (int account = 0; account < accounts[person].length; account++) {
+                sum += accounts[person][account];
+            }
+            if (sum > ans) ans = sum;
+        }
+        return ans;*/
+    }
+
+}
+
+
