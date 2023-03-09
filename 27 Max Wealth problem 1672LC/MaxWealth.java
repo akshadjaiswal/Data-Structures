6c3dfa04b1727857cde0ffddbf816b80c57f154a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-09 17:54:33 +0530

    comment

--- 27 Max Wealth problem 1672LC/MaxWealth.java
+++ 27 Max Wealth problem 1672LC/MaxWealth.java
@@ -2,6 +2,7 @@
 class MaxWealth {
     public int maximumWealth(int[][] accounts) {
        int ans = Integer.MIN_VALUE;
+       //Enhanced for loop 
         for (int[] ints : accounts) {
             int sum = 0;
             for (int anInt : ints) {
