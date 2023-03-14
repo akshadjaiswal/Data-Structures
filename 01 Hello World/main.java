414658b70d8234762d654f76e0e2c8b3900cb066
kunal mirgal <106428665+kunalmirgal@users.noreply.github.com>
2023-03-14 15:14:52 +0530

    contributor statement

--- 01 Hello World/main.java
+++ 01 Hello World/main.java
@@ -2,5 +2,6 @@ package main;
     public class Main {
          public static void main(String[] args) {
          System.out.println("Hello world");
+         System.out.println("I am kunal contributing in this repo");
     }
  }
