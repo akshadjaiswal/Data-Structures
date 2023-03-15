5345722e48e6b2272fdf78f44eb5ebd6b4f3c90a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-15 20:34:22 +0530

    comment

--- for loop java/Main.java
+++ for loop java/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String [] args){
+        //basic looping statement
         for(count =1;count !=5;count++){
             System.out.println(count);
         }
