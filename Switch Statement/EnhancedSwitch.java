9e50118bdfa5a7226cbc4c1394b2279c19c89789
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-15 20:33:25 +0530

    comment

--- Switch Statement/EnhancedSwitch.java
+++ Switch Statement/EnhancedSwitch.java
@@ -5,7 +5,7 @@ public class EnhancedSwitch {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the any fruit from - mango,apple,orange,cherry");
         String fruit = in.next();
-
+        //enhanced switch statements newly introduced by java for better clean code
         switch (fruit) {
             case ("mango") -> System.out.println("King of the fruit");
             case ("apple") -> System.out.println("Doctor recommended");
