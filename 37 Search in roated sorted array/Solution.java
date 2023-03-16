23d61f5465859a879bb8b3722ea4ca5025ea0849
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-16 11:17:25 +0530

    solution leetcode

--- 37 Search in roated sorted array/Solution.java
+++ 37 Search in roated sorted array/Solution.java
@@ -1,10 +1,35 @@
-public class RoatedBinarySearch {
-    public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println(Findpivot(arr));
+class Solution {
+    int search(int[] nums, int target) {
+        int pivot = Findpivot(nums);
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
     }
 
-    static int Findpivot(int[] arr) {
+     int binarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                //Ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+     int Findpivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
@@ -15,7 +40,7 @@ public class RoatedBinarySearch {
             if (mid > start && arr[mid] < arr[mid - 1]) {
                 return mid - 1;
             }
-            if (arr[mid] < arr[start]) {
+            if (arr[mid] <= arr[start]) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
@@ -23,4 +48,5 @@ public class RoatedBinarySearch {
         }
         return -1;
     }
+
 }
