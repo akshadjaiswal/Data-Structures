770ae2026d6acad6eae23e753502a8c441d46d7b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-23 20:51:36 +0530

    condition for already sorted array

--- Sorting/01 Bubble Sort/Main.java
+++ Sorting/01 Bubble Sort/Main.java
@@ -11,7 +11,7 @@ public class Main {
   static void bubble(int[] arr) {
     //run steps n-1 times
     for (int i = 0; i < arr.length; i++) {
-        boolean swapped=false;
+      boolean swapped = false;
       //for each steps max item will come at last index
       for (int j = 1; j <= arr.length - 1; j++) {
         //Swap if the item is smaller than the previous item
@@ -19,13 +19,13 @@ public class Main {
           int temp = arr[j];
           arr[j] = arr[j - 1];
           arr[j - 1] = temp;
-          swapped=true;
+          swapped = true;
         }
       }
-       //if you did not swapped for the particular value of i then it means the array is sorted already then stop.
-            if (!swapped) {
-                break;
-            }
+      //if you did not swapped for the particular value of i then it means the array is sorted already then stop.
+      if (!swapped) {
+        break;
+      }
     }
   }
 }
