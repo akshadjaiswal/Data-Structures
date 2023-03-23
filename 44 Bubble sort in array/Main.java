e3db33b7ff33d73701062e5f1a3f15e70c16b452
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-23 20:58:22 +0530

    Simple bubble sort problem

---
+++ 44 Bubble sort in array/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int[] arr = { 3, 1, 5, 4, 2 };
+    bubble(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  static void bubble(int[] arr) {
+    //run steps n-1 times
+    for (int i = 0; i < arr.length; i++) {
+      boolean swapped = false;
+      //for each steps max item will come at last index
+      for (int j = 1; j <= arr.length - 1; j++) {
+        //Swap if the item is smaller than the previous item
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+          swapped = true;
+        }
+      }
+      //if you did not swapped for the particular value of i then it means the array is sorted already then stop.
+      if (!swapped) {
+        break;
+      }
+    }
+  }
+}
