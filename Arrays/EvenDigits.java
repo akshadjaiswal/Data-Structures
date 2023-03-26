ca2c6281a23e8776dd22293e7790b1064d4cea1b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-26 20:07:52 +0530

    code formatting

--- Arrays/EvenDigits.java
+++ Arrays/EvenDigits.java
@@ -1,49 +1,51 @@
 //https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
 public class EvenDigits {
-    public static void main(String[] args) {
-        int[] nums = {12, 345, 2, 6, 7896};
-       System.out.println(findNumbers(nums));
-        // System.out.println(digits(0));
-    }
 
-    static int findNumbers(int[] nums) {
-        int count = 0;
-        for (int num : nums) {
-            if (even(num)) {
-                count++;
-            }
-        }
-        return count;
-    }
+  public static void main(String[] args) {
+    int[] nums = { 12, 345, 2, 6, 7896 };
+    System.out.println(findNumbers(nums));
+    // System.out.println(digits(0));
+  }
 
-    //function to check wether the number contains even digits or not
-    static boolean even(int num) {
-        int numberOfDigits = digits2(num);
-       /* if (numberOfDigits % 2 == 0) return true;
+  static int findNumbers(int[] nums) {
+    int count = 0;
+    for (int num : nums) {
+      if (even(num)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  //function to check wether the number contains even digits or not
+  static boolean even(int num) {
+    int numberOfDigits = digits2(num);
+    /* if (numberOfDigits % 2 == 0) return true;
         else return false;*/
-        return numberOfDigits % 2 == 0;
+    return numberOfDigits % 2 == 0;
+  }
+
+  //optimized digit finding method
+  static int digits2(int num) {
+    if (num < 0) {
+      num = num * -1;
     }
-    //optimized digit finding method
-    static int digits2(int num) {
-        if (num < 0) {
-            num = num * -1;
-        }
-        return (int) (Math.log10(num)) + 1;
-    }
-    //count the number of the digit
-    // static int digits(int num) {
-    //     if (num < 0) {
-    //         num = num * -1;
-    //     }
-    //     if (num == 0) {
-    //         return 1;
-    //     }
-    //     int count = 0;
-    //     while (num > 0) {
-    //         count++;
-    //         num = num / 10;
-    //     }
-    //     return count;
-    // }
+    return (int) (Math.log10(num)) + 1;
+  }
+  //count the number of the digit
+  // static int digits(int num) {
+  //     if (num < 0) {
+  //         num = num * -1;
+  //     }
+  //     if (num == 0) {
+  //         return 1;
+  //     }
+  //     int count = 0;
+  //     while (num > 0) {
+  //         count++;
+  //         num = num / 10;
+  //     }
+  //     return count;
+  // }
 }
--- Arrays/main.java
+++ Arrays/main.java
@@ -1,35 +1,34 @@
 public class Main {
 
-    public static void main(String[] args) {
-        // Q: store a roll number
-        int a = 19;
+  public static void main(String[] args) {
+    // Q: store a roll number
+    int a = 19;
 
-        // Q: store a person's name
-        String name = "Akshad Jaiswal";
+    // Q: store a person's name
+    String name = "Akshad Jaiswal";
 
-        // Q: store 5 roll numbers
-        int rno1 = 23;
-        int rno2 = 55;
-        int rno3 = 18;
+    // Q: store 5 roll numbers
+    int rno1 = 23;
+    int rno2 = 55;
+    int rno3 = 18;
 
-        // syntax
-        // datatype[] variable_name = new datatype[size];
-        // store 5 roll numbers:
-//        int[] rnos = new int[5];
-//        // or directly
-//        int[] rnos2 = {23, 12, 45, 32, 15};
+    // syntax
+    // datatype[] variable_name = new datatype[size];
+    // store 5 roll numbers:
+    //        int[] rnos = new int[5];
+    //        // or directly
+    //        int[] rnos2 = {23, 12, 45, 32, 15};
 
-        int[] ros; // declaration of array. ros is getting defined in the stack
-        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+    int[] ros; // declaration of array. ros is getting defined in the stack
+    ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
 
-//        System.out.println(ros[1]);
+    //        System.out.println(ros[1]);
 
-        String[] arr = new String[4];
-        System.out.println(arr[0]);
+    String[] arr = new String[4];
+    System.out.println(arr[0]);
+    //        for (String element : arr) {
+    //            System.out.println(element);
+    //        }
 
-//        for (String element : arr) {
-//            System.out.println(element);
-//        }
-
-    }
+  }
 }
