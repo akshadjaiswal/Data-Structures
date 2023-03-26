c41825d198c4fd0704030f8f6fe097ff1588461a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-26 20:04:08 +0530

    example update

--- 44 Bubble sort in array/Main.java
+++ 44 Bubble sort in array/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(String[] args) {
-    int[] arr = { 3, 1, 5, 4, 2 };
+    int[] arr = { 3, 1, 5, 4, 2 ,7,6};
     bubble(arr);
     System.out.println(Arrays.toString(arr));
   }
