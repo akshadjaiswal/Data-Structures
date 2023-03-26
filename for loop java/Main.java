2a97f3d7b94a297eb8bd733c59134b6570f506a9
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-26 20:06:20 +0530

    code formatting

--- for loop java/Main.java
+++ for loop java/Main.java
@@ -1,8 +1,9 @@
-public class Main{
-    public static void main(String [] args){
-        //basic looping statement
-        for(count =1;count !=5;count++){
-            System.out.println(count);
-        }
+public class Main {
+
+  public static void main(String[] args) {
+    //basic looping statement
+    for (count = 1; count != 5; count++) {
+      System.out.println(count);
     }
+  }
 }
