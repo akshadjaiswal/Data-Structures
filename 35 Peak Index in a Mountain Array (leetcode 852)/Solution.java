d06a78a44f9bba9cb457cbb68a390b82e0b7e6e8
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-27 21:54:34 +0530

    code formatting

--- 35 Peak Index in a Mountain Array (leetcode 852)/Solution.java
+++ 35 Peak Index in a Mountain Array (leetcode 852)/Solution.java
@@ -1,16 +1,18 @@
 //https://leetcode.com/problems/peak-index-in-a-mountain-array/
 //leetcode uploaded solution
 class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int start=0;
-        int end = arr.length-1;
-            while(start<end){
-                int mid = start + (end - start) / 2;
-                if(arr[mid]>arr[mid+1]){
-                    end=mid;
-                }else{
-                    start=mid+1;
-                }
-            }return start;
+
+  public int peakIndexInMountainArray(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      if (arr[mid] > arr[mid + 1]) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
     }
+    return start;
+  }
 }
