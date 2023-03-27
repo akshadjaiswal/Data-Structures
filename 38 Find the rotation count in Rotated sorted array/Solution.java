bacb3958ce27fa0b141e30eaeb24e5e57c0fc6c9
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-27 21:55:00 +0530

    code formatting

--- 38 Find the rotation count in Rotated sorted array/Solution.java
+++ 38 Find the rotation count in Rotated sorted array/Solution.java
@@ -1,74 +1,77 @@
 public class Solution {
-    public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 8, 0, 1, 2};
-        System.out.println(FindRotationsCount(arr));
-    }
 
-    private static int FindRotationsCount(int[] arr) {
-        int ans = Findpivot(arr);
-        return ans + 1;
+  public static void main(String[] args) {
+    int[] arr = { 4, 5, 6, 7, 8, 0, 1, 2 };
+    System.out.println(FindRotationsCount(arr));
+  }
 
+  private static int FindRotationsCount(int[] arr) {
+    int ans = Findpivot(arr);
+    return ans + 1;
+  }
+
+  //use when the array contain no duplicates
+  static int Findpivot(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    //find middle element
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      //4 cases here
+      if (mid < end && arr[mid] > arr[mid + 1]) {
+        return mid;
+      }
+      if (mid > start && arr[mid] < arr[mid - 1]) {
+        return mid - 1;
+      }
+      if (arr[mid] <= arr[start]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     }
-    //use when the array contain no duplicates
-    static int Findpivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        //find middle element
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            //4 cases here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+    return -1;
+  }
+
+  //use this when array contain no duplicates
+  static int FindpivotWithDuplicates(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      // 4 cases over here
+      if (mid < end && arr[mid] > arr[mid + 1]) {
+        return mid;
+      }
+      if (mid > start && arr[mid] < arr[mid - 1]) {
+        return mid - 1;
+      }
+
+      // if elements at middle, start, end are equal then just skip the duplicates
+      if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+        // skip the duplicates
+        // NOTE: what if these elements at start and end were the pivot??
+        // check if start is pivot
+        if (start < end && arr[start] > arr[start + 1]) {
+          return start;
         }
-        return -1;
-    }
+        start++;
 
-    //use this when array contain no duplicates
-    static int FindpivotWithDuplicates(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-
-            // if elements at middle, start, end are equal then just skip the duplicates
-            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
-                // skip the duplicates
-                // NOTE: what if these elements at start and end were the pivot??
-                // check if start is pivot
-                if (start < end && arr[start] > arr[start + 1]) {
-                    return start;
-                }
-                start++;
-
-                // check whether end is pivot
-                if (end > start && arr[end] < arr[end - 1]) {
-                    return end - 1;
-                }
-                end--;
-            }
-            // left side is sorted, so pivot should be in right
-            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        // check whether end is pivot
+        if (end > start && arr[end] < arr[end - 1]) {
+          return end - 1;
         }
-        return -1;
+        end--;
+      }
+      // left side is sorted, so pivot should be in right
+      else if (
+        arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])
+      ) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
+    return -1;
+  }
 }
