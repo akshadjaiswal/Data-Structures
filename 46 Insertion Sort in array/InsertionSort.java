85a092a6149f5dd7295c3af7acb0f69508ca33e5
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-29 19:47:21 +0530

    solution

---
+++ 46 Insertion Sort in array/InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+  public static void main(String[] args) {
+    int[] arr1 = { 4, 5, 1, 2, 3 };
+    int[] arr = { 22, 58, 0, 6, -8, -22 };
+    Insertion(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  static void Insertion(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = i + 1; j > 0; j--) {
+        if (arr[j] < arr[j - 1]) {
+          swap(arr, j, j - 1);
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+}
