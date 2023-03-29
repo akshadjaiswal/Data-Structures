ee0a3ebe35d44b8a8c76f71585b67cde9f283c30
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-29 19:39:10 +0530

    example update

--- Sorting/03 Insertion Sort/InsertionSort.java
+++ Sorting/03 Insertion Sort/InsertionSort.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class InsertionSort {
 
   public static void main(String[] args) {
-    int[] arr = { 4, 5, 1, 2, 3 };
+    int[] arr = { 5, 1,4, 2, 3 };
     Insertion(arr);
     System.out.println(Arrays.toString(arr));
   }
 
+  //Iinsertion sort
   static void Insertion(int[] arr) {
     for (int i = 0; i < arr.length - 1; i++) {
       for (int j = i + 1; j > 0; j--) {
@@ -20,6 +21,7 @@ public class InsertionSort {
     }
   }
 
+  //Swaooing
   static void swap(int[] arr, int first, int second) {
     int temp = arr[first];
     arr[first] = arr[second];
