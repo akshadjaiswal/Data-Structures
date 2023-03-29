9dcf54bf6d172b49059889db817b6e49dcf5f470
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-29 19:38:19 +0530

    formatting

--- Sorting/03 Insertion Sort/InsertionSort.java
+++ Sorting/03 Insertion Sort/InsertionSort.java
@@ -1,27 +1,28 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    public static void main(String[] args) {
-        int[] arr = {4, 5, 1, 2, 3};
-        Insertion(arr);
-        System.out.println(Arrays.toString(arr));
 
-    }
+  public static void main(String[] args) {
+    int[] arr = { 4, 5, 1, 2, 3 };
+    Insertion(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 
-    static void Insertion(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    swap(arr, j, j - 1);
-                } else {
-                    break;
-                }
-            }
+  static void Insertion(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = i + 1; j > 0; j--) {
+        if (arr[j] < arr[j - 1]) {
+          swap(arr, j, j - 1);
+        } else {
+          break;
         }
+      }
     }
-     static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
 }
