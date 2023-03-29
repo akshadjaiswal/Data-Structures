6d00dad36082598ce5f3c06960b26e5c1f36ace6
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-03-29 19:37:25 +0530

    Insertion function

---
+++ Sorting/03 Insertion Sort/InsertionSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 2, 3};
+        Insertion(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void Insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
