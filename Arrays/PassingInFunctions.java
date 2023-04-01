ecf41379c2024c8f4d866a18382e4e545b4f357b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-01 22:17:01 +0530

    code formatting

--- Arrays/PassingInFunctions.java
+++ Arrays/PassingInFunctions.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
 
 public class PassingInFunctions {
-    public static void main(String[] args) {
-        int[] num = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(num));
-        change(num);
-        System.out.println(Arrays.toString(num));
-    }
 
-    static void change(int arr[]) {
-        arr[0] = 99;
-    }
+  public static void main(String[] args) {
+    int[] num = { 1, 2, 3, 4, 5 };
+    System.out.println(Arrays.toString(num));
+    change(num);
+    System.out.println(Arrays.toString(num));
+  }
+
+  static void change(int arr[]) {
+    arr[0] = 99;
+  }
 }
