f5ffc65756a70f21b2eb4ae629116a5d845c06a0
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-02 15:49:13 +0530

    Set Miss Match question and solution

---
+++ Sorting/04 Cycle Sort/SetMissmatch.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/set-mismatch/
+import java.util.Arrays;
+
+public class SetMissmatch {
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 2, 4 };
+    int[] ans = findErrorNums(nums);
+    System.out.println(Arrays.toString(ans));
+  }
+
+  static int[] findErrorNums(int[] nums) {
+    int i = 0;
+    while (i < nums.length) {
+      int correct = nums[i] - 1;
+      if (nums[i] != nums[correct]) {
+        swap(nums, i, correct);
+      } else {
+        i++;
+      }
+    }
+    //find missing number
+    for (int index = 0; index < nums.length; index++) {
+      if (nums[index] != index + 1) {
+        return new int[] { nums[index], index + 1 };
+      }
+    }
+    return new int[] { -1, -1 };
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+}
