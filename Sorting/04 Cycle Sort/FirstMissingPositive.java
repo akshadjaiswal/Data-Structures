1f0ad7e2ad7082d681d73dfc46d8e0fb374f6c8d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-04 21:35:31 +0530

    First missing positive number code

---
+++ Sorting/04 Cycle Sort/FirstMissingPositive.java
@@ -0,0 +1,33 @@
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+
+        int[] arr = {3, 4, -1, 1};
+        int ans = FirstMissingPositive(arr);
+        System.out.println(ans);
+    }
+
+    public static int FirstMissingPositive(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        //finding first  missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return index + 1;
+            }
+        }
+        return arr.length + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
