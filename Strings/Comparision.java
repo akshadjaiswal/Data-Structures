c4531e8dbdd805b525e6bf0b5aa46c8fef091c8d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-07 10:31:07 +0530

    comparison

--- Strings/Comparision.java
+++ Strings/Comparision.java
@@ -1,8 +1,14 @@
 public class Comparision {
-    public static void main(String[] args) {
-        String a = "Akshad";
-        String b = "Akshad";
-        //==
-        System.out.println(a == b);
-    }
+
+  public static void main(String[] args) {
+    String a = "Akshad";
+    String b = "Akshad";
+    //==
+    System.out.println(a == b);
+    String name1 = new String("Akshad");
+    String name2 = new String("Akshad");
+    //System.out.println(name1==name2); //comparator
+    System.out.println(name1.equals(name2)); //method
+    System.out.println(name1.charAt(0));
+  }
 }
