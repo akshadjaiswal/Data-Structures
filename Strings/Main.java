83f6b31fb5b331df777f85f953f88405250d5c72
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-07 10:03:13 +0530

    immutability

--- Strings/Main.java
+++ Strings/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
     int[] arr = { 1, 2, 3, 4, 5 };
-    int a = 0;
-    String k = "Akshad";
+    int num = 0;
+
+    String a = "Akshad";
+    System.out.println(a);
+    a = "Jaiswal";
+    System.out.println(a);
   }
 }
