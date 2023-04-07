66672fd203d29e80aaae554c325724cc98f2844a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-07 10:12:34 +0530

    String comparison

---
+++ Strings/Comparision.java
@@ -0,0 +1,8 @@
+public class Comparision {
+    public static void main(String[] args) {
+        String a = "Akshad";
+        String b = "Akshad";
+        //==
+        System.out.println(a == b);
+    }
+}
