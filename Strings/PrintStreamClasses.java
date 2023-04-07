e23c04be292304498517cacb64cc1e8ddb0f0583
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-07 11:29:55 +0530

    printstream classes

---
+++ Strings/PrintStreamClasses.java
@@ -0,0 +1,11 @@
+public class PrintStreamClasses {
+
+  public static void main(String[] args) {
+    System.out.println(56);
+    System.out.println("Akshad");
+    System.out.println(new int[] { 1, 32 });
+
+    String name = null;
+    System.out.println(name);
+  }
+}
