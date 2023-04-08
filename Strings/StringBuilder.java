139588740a995fe600f378f24a515451090eef82
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-08 11:12:01 +0530

    String Builder class

---
+++ Strings/StringBuilder.java
@@ -0,0 +1,15 @@
+public class StringBuilder {
+
+  public static void main(String[] args) {
+    StringBuilder Builder = new StringBuilder();
+    for (int i = 0; i < 26; i++) {
+      char ch = (char) ('a' + i);
+      Builder.append(ch);
+    }
+    System.out.println(Builder.toString());
+    //Builder.deleteCharAt(0);
+    //System.out.println(Builder.toString());
+    Builder.reverse();
+    System.out.println(Builder.toString());
+  }
+}
