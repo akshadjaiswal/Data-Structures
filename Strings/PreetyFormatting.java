5b21b93e277cf742c1e6795ea6c304458ca7fc2c
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-08 10:03:58 +0530

    Pretty Formatting in java

---
+++ Strings/PreetyFormatting.java
@@ -0,0 +1,15 @@
+public class PreetyPrinting {
+
+  public static void main(String[] args) {
+    float f = 2.3453f;
+    //        System.out.printf("Float number is %.2f ", f);
+
+    //        System.out.printf("Pie value : %.3f", Math.PI);
+
+    System.out.printf(
+      "Hello my name is %s and I am a %s boy.",
+      "Akshad",
+      "good"
+    );
+  }
+}
