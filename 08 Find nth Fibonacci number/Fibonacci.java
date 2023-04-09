a3b6743f8bc6066a6fc4225b173da094dca8174f
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-09 13:16:44 +0530

    code formatting

--- 08 Find nth Fibonacci number/Fibonacci.java
+++ 08 Find nth Fibonacci number/Fibonacci.java
@@ -3,20 +3,21 @@ package com.akshad;
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        //Q.Find the nth fibonacci number
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the nth number of fibonacci series-");
-        int n = in.nextInt();
-        int a = 0;
-        int b = 1;
-        int count = 2;
-        while (count <= n) {
-            int temp = b;
-            b = b + a;
-            a = temp;
-            count++;
-        }
-        System.out.println(b);
+
+  public static void main(String[] args) {
+    //Q.Find the nth fibonacci number
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the nth number of fibonacci series-");
+    int n = in.nextInt();
+    int a = 0;
+    int b = 1;
+    int count = 2;
+    while (count <= n) {
+      int temp = b;
+      b = b + a;
+      a = temp;
+      count++;
     }
+    System.out.println(b);
+  }
 }
