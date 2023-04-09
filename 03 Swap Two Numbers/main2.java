41f1f3374493df5049094678f247e3cfe33a799e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-09 13:14:20 +0530

    code formatting

--- 03 Swap Two Numbers/main2.java
+++ 03 Swap Two Numbers/main2.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-class main2{
-    public static void main(String args[]){
-        int x,y;
-        System.out.println("Enter x and y");
-        Scanner in=new Scanner(System.in);
 
-        x=in.nextInt();
-        y=in.nextInt();
-        //Before swapping values of x and y
-        System.out.println("Before Swapping\nx="+x+"\ny="+y);
+class main2 {
 
-        x=x+y;
-        y=x-y;
-        x=x-y;
-        //After swapping values
-        System.out.println("After Swapping Swapping\nx="+x+"\ny="+y);
-    }
+  public static void main(String args[]) {
+    int x, y;
+    System.out.println("Enter x and y");
+    Scanner in = new Scanner(System.in);
+
+    x = in.nextInt();
+    y = in.nextInt();
+    //Before swapping values of x and y
+    System.out.println("Before Swapping\nx=" + x + "\ny=" + y);
+
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    //After swapping values
+    System.out.println("After Swapping Swapping\nx=" + x + "\ny=" + y);
+  }
 }
