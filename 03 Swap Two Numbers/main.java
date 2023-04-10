240265a2e9c39847fe81e5219fa6838274d8b662
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-10 20:40:52 +0530

    update

--- 03 Swap Two Numbers/main.java
+++ 03 Swap Two Numbers/main.java
@@ -1,12 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        System.out.println("Before swaping a="+a+ " b="+b);
 
-        int temp=a;
-        a=b;
-        b = temp;
-        System.out.println("After swaping a="+a+ " b="+b);
-    }
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    System.out.println("Before swaping a=" + a + " b=" + b);
+
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("After swaping a=" + a + " b=" + b);
+  }
 }
