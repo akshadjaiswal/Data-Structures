2328b4d6d254da79ae907827ddc77d57f3ebd0f6
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-10 20:45:23 +0530

    code formatting

--- Switch Statement/Switch.java
+++ Switch Statement/Switch.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class Switch {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the any fruit from - mango,apple,orange,cherry");
-        String fruit = in.next();
-        //Switch statements
-        switch (fruit) {
-            case ("mango"):
-                System.out.println("King of the fruit");
-                break;
-            case ("apple"):
-                System.out.println("Doctor recommended");
-                break;
-            case ("orange"):
-                System.out.println("Sweet fruit");
-                break;
-            case ("cherry"):
-                System.out.println("Small fruit");
-                break;
-            //if nothing executes the  execute this default thing
-            default:
-                System.out.println("Enter Valid fruit");
-                break;
-        }
 
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the any fruit from - mango,apple,orange,cherry");
+    String fruit = in.next();
+    //Switch statements
+    switch (fruit) {
+      case ("mango"):
+        System.out.println("King of the fruit");
+        break;
+      case ("apple"):
+        System.out.println("Doctor recommended");
+        break;
+      case ("orange"):
+        System.out.println("Sweet fruit");
+        break;
+      case ("cherry"):
+        System.out.println("Small fruit");
+        break;
+      //if nothing executes the  execute this default thing
+      default:
+        System.out.println("Enter Valid fruit");
+        break;
     }
+  }
 }
