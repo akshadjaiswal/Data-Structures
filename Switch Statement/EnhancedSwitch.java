9c17239886d91343e5467dd3e2e4388629d80975
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-10 20:45:01 +0530

    code formatting

--- Switch Statement/EnhancedSwitch.java
+++ Switch Statement/EnhancedSwitch.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class EnhancedSwitch {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the any fruit from - mango,apple,orange,cherry");
-        String fruit = in.next();
-        //enhanced switch statements newly introduced by java for better clean code
-        switch (fruit) {
-            case ("mango") -> System.out.println("King of the fruit");
-            case ("apple") -> System.out.println("Doctor recommended");
-            case ("orange") -> System.out.println("Sweet fruit");
-            case ("cherry") -> System.out.println("Small fruit");
-            default -> System.out.println("Enter Valid fruit");
-        }
 
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the any fruit from - mango,apple,orange,cherry");
+    String fruit = in.next();
+    //enhanced switch statements newly introduced by java for better clean code
+    switch (fruit) {
+      case ("mango") -> System.out.println("King of the fruit");
+      case ("apple") -> System.out.println("Doctor recommended");
+      case ("orange") -> System.out.println("Sweet fruit");
+      case ("cherry") -> System.out.println("Small fruit");
+      default -> System.out.println("Enter Valid fruit");
     }
+  }
 }
