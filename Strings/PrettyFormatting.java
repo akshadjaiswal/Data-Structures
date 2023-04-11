a58bd8a88e37a75c0474ca0d421b340c269a93ac
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-11 20:57:56 +0530

    update

--- Strings/PrettyFormatting.java
+++ Strings/PrettyFormatting.java
@@ -2,9 +2,8 @@ public class PrettyPrinting {
 
   public static void main(String[] args) {
     float f = 2.3453f;
-    //        System.out.printf("Float number is %.2f ", f);
-
-    //        System.out.printf("Pie value : %.3f", Math.PI);
+    //System.out.printf("Float number is %.2f ", f);
+    //System.out.printf("Pie value : %.3f", Math.PI);
 
     System.out.printf(
       "Hello my name is %s and I am a %s boy.",
