2ae588b6fa657411d46a9ace4f0b6599758f248d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-12 10:28:32 +0530

    Last Index Example

--- Strings/LastIndexExample.java
+++ Strings/LastIndexExample.java
@@ -1,10 +1,12 @@
-public class LastIndexExample{ 
-public static void main(String args[]){ 
-String s1="hello"; 
-String s2="hello"; 
-String s3="meklo"; 
-String s4="hemlo"; 
-System.out.println(s1.compareTo(s2)); 
-System.out.println(s1.compareTo(s3)); 
-System.out.println(s1.compareTo(s4)); 
-}} 
+public class LastIndexExample {
+
+  public static void main(String args[]) {
+    String s1 = "hello";
+    String s2 = "hello";
+    String s3 = "meklo";
+    String s4 = "hemlo";
+    System.out.println(s1.compareTo(s2));
+    System.out.println(s1.compareTo(s3));
+    System.out.println(s1.compareTo(s4));
+  }
+}
