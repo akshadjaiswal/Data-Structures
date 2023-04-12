d28ad5213c4e6ddcd1446020db17288c24ff1767
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-12 10:31:41 +0530

    update

--- Strings/ReplaceAllExample.java
+++ Strings/ReplaceAllExample.java
@@ -2,7 +2,7 @@ public class ReplaceAllExample {
 
   public static void main(String args[]) {
     String s1 =
-      "My name is Rajendra. My name is lamror. My name is Technolamror.";
+      "My name is Akshad. My name is Mint. My name is Akshuu.";
     String replaceString = s1.replaceAll("is", "was"); //replaces all occurrences of "is" to "was"
 
     System.out.println(replaceString);
