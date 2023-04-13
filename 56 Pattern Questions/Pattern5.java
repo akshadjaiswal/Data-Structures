aee84a46bbace69fa697f16d94c28e818b95944a
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-13 17:27:32 +0530

    Pattern Printing

---
+++ 56 Pattern Questions/Pattern5.java
@@ -0,0 +1,16 @@
+public class Pattern5 {
+
+  public static void main(String[] args) {
+    pattern(5);
+  }
+
+  static void pattern(int n) {
+    for (int row = 0; row < 2 * n; row++) {
+      int TotalNoOfColoumns = row > n ? 2 * n - row : row;
+      for (int col = 0; col < TotalNoOfColoumns; col++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
